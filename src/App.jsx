6c9b3ec490a686aa7af8b633433fb15c9a16ee95
Patrick Thesken <patrick@student.dev-island.org>
2024-05-07 18:06:30 -1000

    jump-to button, nav bar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,78 @@
-import { useState } from "react";
 import "normalize.css";
 import "./App.css";
-import Logo from "./assets/images/logo.png";
+import Logo from "./assets/images/logo2.png";
 import Arrangement from "./assets/images/arrangement.jpg";
+import Image1 from "./assets/images/image1.jpeg";
+import Image2 from "./assets/images/image2.jpg";
+import Image3 from "./assets/images/image3.jpg";
+import Image4 from "./assets/images/image4.jpg";
+//why is one jpeg rest are jpg?
+import Form from "./Components/Form";
+import { FaInstagramSquare } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { IoLogoVenmo } from "react-icons/io5";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Autoplay,
+  Pagination,
+  Navigation,
+  Mousewheel,
+  Keyboard,
+} from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function App() {
+  // Create a reference to the Form
+  const formRef = useRef(null);
+
+  // Function to scroll to the Form component
+  const scrollToForm = () => {
+    formRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="nav-bar">
-        <div className="logo">
-          <img src={Logo} alt="Logo" />
+        <div className="nav-bar-contents">
+          <div className="icons-container">
+            <div className="contact-icons">
+              <a href="mailto:tuni0627@icloud.com" title="Send us an email">
+                <MdEmail />
+              </a>
+              <a
+                href="https://www.instagram.com/dagreenrainbowdesignz"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Visit our Instagram"
+              >
+                <FaInstagramSquare />
+              </a>
+              <a
+                href="https://venmo.com/yourusername"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Visit our Venmo"
+              >
+                <IoLogoVenmo />
+              </a>
+            </div>
+          </div>
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+          <div className="hamburger-container"></div>
         </div>
-        <div className="contact-icons">icons</div>
+        <div className="nav-bar-form-button-container">
+          <button className="nav-bar-form-button" onClick={scrollToForm}>
+            Plan my party
+          </button>
+        </div>
       </div>
       <div className="content-container">
         <div className="opening-photo-container">
@@ -28,8 +89,37 @@ function App() {
             services for events on Kauai.
           </p>
         </div>
-        <div className="form">form</div>
-        <div className="photos">photos</div>
+
+        <Form formRef={formRef} />
+
+        <div className="photos">
+          <Swiper
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            cssMode={true}
+            navigation={true}
+            pagination={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Autoplay, Navigation, Pagination, Mousewheel, Keyboard]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={Image1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Image2} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Image3} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Image4} />
+            </SwiperSlide>
+          </Swiper>
+        </div>
         <div className="footer">
           footer
           <div className="contact-icons">icons</div>
---
+++ src/Components/Form.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+export default function Form({ formRef }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [budget, setBudget] = useState("");
+  const [details, setDetails] = useState("");
+  const [assist, setAssist] = useState("");
+
+  return (
+    <form className="form-container" ref={formRef}>
+      <label className="form-item">
+        Your Name
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setName(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Phone Number
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setPhone(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Email
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setEmail(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Event Address
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setAddress(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Event Date
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setDate(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Event Time
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setTime(value);
+          }}
+        />
+      </label>
+
+      <label className="form-item">
+        Your Budget
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setBudget(value);
+          }}
+        />
+      </label>
+      <label className="form-item">
+        More event details
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setDetails(value);
+          }}
+        />
+      </label>
+      <label className="form-item">
+        How can we assist?
+        <input
+          type="text"
+          onChange={(event) => {
+            const value = event.target.value;
+            setAssist(value);
+          }}
+        />
+      </label>
+
+      <button
+        type="submit"
+        onClick={(event) => {
+          event.preventDefault();
+          const newBook = {
+            title: bookTitle,
+            author,
+            genre,
+            description,
+            image: bookImage.trim() ? bookImage : BookImage, //ternary: if true whats after "?", if not then what's after the colon
+          };
+          //move to new function? ^
+
+          setBookList([...bookList, newBook]);
+        }}
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
