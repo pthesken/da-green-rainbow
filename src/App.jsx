30d35e390f7dfc3730d48892ee20730305b14650
Patrick Thesken <patrick@student.dev-island.org>
2024-06-02 18:11:55 -1000

    fixed page margin issue

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,16 @@ function App() {
   const swiperRef = useRef(null);
   const [activeSlide, setActiveSlide] = useState(0);
 
+  const slideLeft = () => {
+    swiperRef.current.swiper.slidePrev();
+    setActiveSlide(swiperRef.current.swiper.realIndex);
+  };
+
+  const slideRight = () => {
+    swiperRef.current.swiper.slideNext();
+    setActiveSlide(swiperRef.current.swiper.realIndex);
+  };
+
   const goToSlide = (index) => {
     if (swiperRef.current && swiperRef.current.swiper) {
       swiperRef.current.swiper.slideTo(index);
@@ -46,134 +56,144 @@ function App() {
         onSlideChange={handleSlideChange}
       >
         <SwiperSlide id="home">
-          <div
-            className="slide-content"
-            style={{ backgroundImage: `url(${Arrangement})` }}
-          >
-            <div className="home-slide-click-contents">
-              <div className="home-slide-body-text">
-                <div className="title" data-swiper-parallax="-300">
-                  Plan My Party
+          {activeSlide === 0 && (
+            <div
+              className="slide-content"
+              style={{ backgroundImage: `url(${Arrangement})` }}
+            >
+              <div className="home-slide-click-contents">
+                <div className="home-slide-body-text">
+                  <div className="title" data-swiper-parallax="-300">
+                    Plan My Party
+                  </div>
+                  <div className="subtitle" data-swiper-parallax="-200">
+                    Make your events memorable
+                  </div>
                 </div>
-                <div className="subtitle" data-swiper-parallax="-200">
-                  Make your events memorable
+                <div className="text" data-swiper-parallax="-100">
+                  <button onClick={() => goToSlide(3)}>Plan My Party</button>
+                </div>
+                <div className="social-icons" data-swiper-parallax="-100">
+                  <a
+                    href="https://www.instagram.com/dagreenrainbowdesignz"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaInstagramSquare size={30} />
+                  </a>
+                  <a
+                    href="mailto:tuni0627@icloud.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <MdEmail size={30} />
+                  </a>
+                  <a
+                    href="https://venmo.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IoLogoVenmo size={30} />
+                  </a>
                 </div>
               </div>
-              <div className="text" data-swiper-parallax="-100">
-                <button onClick={() => goToSlide(3)}>Plan My Party</button>
-              </div>
-              <div className="social-icons" data-swiper-parallax="-100">
-                <a
-                  href="https://www.instagram.com/dagreenrainbowdesignz"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaInstagramSquare size={30} />
-                </a>
-                <a
-                  href="mailto:tuni0627@icloud.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <MdEmail size={30} />
-                </a>
-                <a
-                  href="https://venmo.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <IoLogoVenmo size={30} />
-                </a>
+              <div className="navigation-arrows">
+                <IoIosArrowBack
+                  className="arrow arrow-left"
+                  onClick={slideLeft}
+                />
+                <IoIosArrowForward
+                  className="arrow arrow-right"
+                  onClick={slideRight}
+                />
               </div>
             </div>
-            <div className="navigation-arrows">
-              <IoIosArrowBack
-                className="arrow arrow-left"
-                onClick={() => swiperRef.current.swiper.slidePrev()}
-              />
-              <IoIosArrowForward
-                className="arrow arrow-right"
-                onClick={() => swiperRef.current.swiper.slideNext()}
-              />
-            </div>
-          </div>
+          )}
         </SwiperSlide>
+
         <SwiperSlide id="about" className="about-slide">
-          <div
-            className="slide-content"
-            style={{ backgroundImage: `url(${Image1})` }}
-          >
-            <div className="about-text-content">
-              <div className="title" data-swiper-parallax="-300">
-                <h2>About Us</h2>
+          {activeSlide === 1 && (
+            <div
+              className="slide-content"
+              style={{ backgroundImage: { Image1 } }}
+            >
+              <div className="about-text-content">
+                <div className="title" data-swiper-parallax="-300">
+                  <h2>About Us</h2>
+                </div>
+                <div className="text" data-swiper-parallax="-100">
+                  <p>
+                    At DA GREEN RAINBOW DESIGNZ, we aim to design with
+                    creativity and aloha spirit through our distinctive
+                    decorations. Whether you're a local family, a visiting
+                    tourist, or a business seeking to elevate your event, we are
+                    dedicated to making your vision a reality with passion,
+                    professionalism, and the unmistakable spirit of Kauai. We
+                    offer a wide variety of balloon backdrops, floral creations,
+                    party or wedding decorating services for any Kauai event!
+                  </p>
+                </div>
               </div>
-              <div className="text" data-swiper-parallax="-100">
-                <p>
-                  At DA GREEN RAINBOW DESIGNZ, we aim to design with creativity
-                  and aloha spirit through our distinctive decorations. Whether
-                  you're a local family, a visiting tourist, or a business
-                  seeking to elevate your event, we are dedicated to making your
-                  vision a reality with passion, professionalism, and the
-                  unmistakable spirit of Kauai. We offer a wide variety of
-                  balloon backdrops, floral creations, party or wedding
-                  decorating services for any Kauai event!
-                </p>
+              <div className="navigation-arrows">
+                <IoIosArrowBack
+                  className="arrow arrow-left"
+                  onClick={slideLeft}
+                />
+                <IoIosArrowForward
+                  className="arrow arrow-right"
+                  onClick={slideRight}
+                />
               </div>
             </div>
-            <div className="navigation-arrows">
-              <IoIosArrowBack
-                className="arrow arrow-left"
-                onClick={() => swiperRef.current.swiper.slidePrev()}
-              />
-              <IoIosArrowForward
-                className="arrow arrow-right"
-                onClick={() => swiperRef.current.swiper.slideNext()}
-              />
-            </div>
-          </div>
+          )}
         </SwiperSlide>
+
         <SwiperSlide id="gallery">
-          <div className="slide-content no-bg-slide">
-            <div className="title" data-swiper-parallax="-300">
-              Gallery
+          {activeSlide === 2 && (
+            <div className="slide-content no-bg-slide">
+              <div className="title" data-swiper-parallax="-300">
+                Gallery
+              </div>
+              <div className="image-gallery-grid" data-swiper-parallax="-100">
+                <img src={Image1} alt="Gallery 1" />
+                <img src={Image2} alt="Gallery 2" />
+                <img src={Image3} alt="Gallery 3" />
+                <img src={Image4} alt="Gallery 4" />
+              </div>
+              <div className="navigation-arrows">
+                <IoIosArrowBack
+                  className="arrow arrow-left"
+                  onClick={slideLeft}
+                />
+                <IoIosArrowForward
+                  className="arrow arrow-right"
+                  onClick={slideRight}
+                />
+              </div>
             </div>
-            <div className="image-gallery-grid" data-swiper-parallax="-100">
-              <img src={Image1} alt="Gallery 1" />
-              <img src={Image2} alt="Gallery 2" />
-              <img src={Image3} alt="Gallery 3" />
-              <img src={Image4} alt="Gallery 4" />
-            </div>
-            <div className="navigation-arrows">
-              <IoIosArrowBack
-                className="arrow arrow-left"
-                onClick={() => swiperRef.current.swiper.slidePrev()}
-              />
-              <IoIosArrowForward
-                className="arrow arrow-right"
-                onClick={() => swiperRef.current.swiper.slideNext()}
-              />
-            </div>
-          </div>
+          )}
         </SwiperSlide>
         <SwiperSlide id="contact" className="no-bg-slide">
-          <div className="slide-content">
-            <div className="title" data-swiper-parallax="-300">
-              Contact Us
+          {activeSlide === 3 && (
+            <div className="slide-content">
+              <div className="title" data-swiper-parallax="-300">
+                Contact Us
+              </div>
+              <div className="text" data-swiper-parallax="-100">
+                <Form />
+              </div>
+              <div className="navigation-arrows">
+                <IoIosArrowBack
+                  className="arrow arrow-left"
+                  onClick={slideLeft}
+                />
+                <IoIosArrowForward
+                  className="arrow arrow-right"
+                  onClick={slideRight}
+                />
+              </div>
             </div>
-            <div className="text" data-swiper-parallax="-100">
-              <Form />
-            </div>
-            <div className="navigation-arrows">
-              <IoIosArrowBack
-                className="arrow arrow-left"
-                onClick={() => swiperRef.current.swiper.slidePrev()}
-              />
-              <IoIosArrowForward
-                className="arrow arrow-right"
-                onClick={() => swiperRef.current.swiper.slideNext()}
-              />
-            </div>
-          </div>
+          )}
         </SwiperSlide>
       </Swiper>
 
